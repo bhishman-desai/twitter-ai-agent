fd99795b84e8d4f2a3df3bfe54aff8adefbdec48
Bhishman Desai <desaibhishman1@gmail.com>
2025-03-03 18:43:29 -0400

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:9000',
-    'https://twitter-ai-agent-bot.vercel.app:8999'
+    'https://twitter-ai-agent-bot.vercel.app'
 ];
 
 module.exports = allowedOrigins;
